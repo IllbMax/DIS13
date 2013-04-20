a4c308ab15fe0f7c090d8be76de88ff8e5a46a23
Max Menzel <catch4all@abwesend.de>
2013-04-20 21:49:48 +0200

    Immobilie(klasse) impl

---
+++ Aufgabe 2 eigener Start/src/ersteVersuche/Material/Immobilie.java
@@ -0,0 +1,111 @@
+package ersteVersuche.Material;
+
+public abstract class Immobilie {
+	private int _id;
+	private String _ort;
+	private String _plz;
+	private String _strasse;
+	private String _hausNr;
+	private float _flaeche;
+
+	public Immobilie(int id, String ort, String plz, String strasse,
+			String hausNr, float flaeche) {
+		_id = id;
+		_ort = ort;
+		_plz = plz;
+		_strasse = strasse;
+		_hausNr = hausNr;
+		_flaeche = flaeche;
+	}
+
+	/**
+	 * @return the iD
+	 */
+	public int getID() {
+		return _id;
+	}
+
+	/**
+	 * @param iD
+	 *            the iD to set
+	 */
+	public void setID(int iD) {
+		_id = iD;
+	}
+
+	/**
+	 * @return the ort
+	 */
+	public String getOrt() {
+		return _ort;
+	}
+
+	/**
+	 * @param ort
+	 *            the ort to set
+	 */
+	public void setOrt(String ort) {
+		_ort = ort;
+	}
+
+	/**
+	 * @return the pLZ
+	 */
+	public String getPLZ() {
+		return _plz;
+	}
+
+	/**
+	 * @param pLZ
+	 *            the pLZ to set
+	 */
+	public void setPLZ(String pLZ) {
+		_plz = pLZ;
+	}
+
+	/**
+	 * @return the strasse
+	 */
+	public String getStrasse() {
+		return _strasse;
+	}
+
+	/**
+	 * @param strasse
+	 *            the strasse to set
+	 */
+	public void setStrasse(String strasse) {
+		_strasse = strasse;
+	}
+
+	/**
+	 * @return the hausNr
+	 */
+	public String getHausNr() {
+		return _hausNr;
+	}
+
+	/**
+	 * @param hausNr
+	 *            the hausNr to set
+	 */
+	public void setHausNr(String hausNr) {
+		_hausNr = hausNr;
+	}
+
+	/**
+	 * @return the flaeche
+	 */
+	public float getFlaeche() {
+		return _flaeche;
+	}
+
+	/**
+	 * @param flaeche
+	 *            the flaeche to set
+	 */
+	public void setFlaeche(float flaeche) {
+		_flaeche = flaeche;
+	}
+
+}
---
+++ Aufgabe 2 eigener Start/src/ersteVersuche/Material/Wohnung.java
@@ -0,0 +1,98 @@
+package ersteVersuche.Material;
+
+public class Wohnung extends Immobilie {
+
+	private int _stockwerk;
+	private float _mietpreis;
+	private int _zimmer;
+	private boolean _balkon;
+	private boolean _ebk;
+
+	public Wohnung(int id, String ort, String plz, String strasse,
+			String hausNr, float flaeche, int stockwerk, float mietpreis,
+			int zimmer, boolean balkon, boolean ebk) {
+		super(id, ort, plz, strasse, hausNr, flaeche);
+
+		_stockwerk = stockwerk;
+		_mietpreis = mietpreis;
+		_zimmer = zimmer;
+		_balkon = balkon;
+		_ebk = ebk;
+	}
+
+	/**
+	 * @return the stockwerk
+	 */
+	public int getStockwerk() {
+		return _stockwerk;
+	}
+
+	/**
+	 * @param stockwerk
+	 *            the stockwerk to set
+	 */
+	public void setStockwerk(int stockwerk) {
+		_stockwerk = stockwerk;
+	}
+
+	/**
+	 * @return the mietpreis
+	 */
+	public float getMietpreis() {
+		return _mietpreis;
+	}
+
+	/**
+	 * @param mietpreis
+	 *            the mietpreis to set
+	 */
+	public void setMietpreis(float mietpreis) {
+		_mietpreis = mietpreis;
+	}
+
+	/**
+	 * @return the zimmer
+	 */
+	public int getZimmer() {
+		return _zimmer;
+	}
+
+	/**
+	 * @param zimmer
+	 *            the zimmer to set
+	 */
+	public void setZimmer(int zimmer) {
+		_zimmer = zimmer;
+	}
+
+	/**
+	 * @return the balkon
+	 */
+	public boolean isBalkon() {
+		return _balkon;
+	}
+
+	/**
+	 * @param balkon
+	 *            the balkon to set
+	 */
+	public void setBalkon(boolean balkon) {
+		_balkon = balkon;
+	}
+
+	/**
+	 * @return the eBK
+	 */
+	public boolean isEBK() {
+		return _ebk;
+	}
+
+	/**
+	 * @param eBK
+	 *            the eBK to set
+	 */
+	public void setEBK(boolean eBK) {
+		_ebk = eBK;
+	}
+
+}
