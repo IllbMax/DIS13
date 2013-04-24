f2d2cd719db27e24823a557037ae539b8c740e89
Max Menzel <catch4all@abwesend.de>
2013-04-24 09:02:28 +0200

    Vertagsklassen hinzugefuegt

---
+++ Aufgabe 2 eigener Start/src/ersteVersuche/Material/Kaufvertrag.java
@@ -0,0 +1,71 @@
+package ersteVersuche.Material;
+
+import java.util.Date;
+
+public class Kaufvertrag extends Vertrag {
+
+	private int _anzahlRaten;
+	private float _ratenzins;
+
+	public Kaufvertrag(int vertragsnr, Date datum, String ort, Person person,
+			Haus immobilie, int anzahlRaten, float ratenzins) {
+		super(vertragsnr, datum, ort, person, immobilie);
+
+		_anzahlRaten = anzahlRaten;
+		_ratenzins = ratenzins;
+	}
+
+	/**
+	 * @return the anzahlRaten
+	 */
+	public int getAnzahlRaten() {
+		return _anzahlRaten;
+	}
+
+	/**
+	 * @param anzahlRaten
+	 *            the anzahlRaten to set
+	 */
+	public void setAnzahlRaten(int anzahlRaten) {
+		this._anzahlRaten = anzahlRaten;
+	}
+
+	/**
+	 * @return the ratentins
+	 */
+	public float getRatenzins() {
+		return _ratenzins;
+	}
+
+	/**
+	 * @param ratentins
+	 *            the ratentins to set
+	 */
+	public void setRatenzins(float ratenzins) {
+		this._ratenzins = ratenzins;
+	}
+
+	/**
+	 * @return the immobilie
+	 */
+	@Override
+	public Haus getImmobilie() {
+		return (Haus) _immobilie;
+	}
+
+	/**
+	 * @return the haus
+	 */
+	public Haus getHaus() {
+		return (Haus) _immobilie;
+	}
+
+	/**
+	 * @param immobilie
+	 *            the immobilie to set
+	 */
+	public void setHaus(Haus haus) {
+		this._immobilie = haus;
+	}
+
+}
---
+++ Aufgabe 2 eigener Start/src/ersteVersuche/Material/Mietvertrag.java
@@ -0,0 +1,88 @@
+package ersteVersuche.Material;
+
+import java.util.Date;
+
+public class Mietvertrag extends Vertrag {
+
+	private Date _mietbeginn;
+	private int _dauer;
+	private float _nebenkosten;
+
+	public Mietvertrag(int vertragsnr, Date datum, String ort, Person person,
+			Wohnung immobilie, Date mietbeginn, int dauer, float nebenkosten) {
+		super(vertragsnr, datum, ort, person, immobilie);
+
+		_mietbeginn = mietbeginn;
+		_dauer = dauer;
+		_nebenkosten = nebenkosten;
+	}
+
+	/**
+	 * @return the immobilie
+	 */
+	@Override
+	public Wohnung getImmobilie() {
+		return (Wohnung) _immobilie;
+	}
+
+	/**
+	 * @return the haus
+	 */
+	public Wohnung getWohnung() {
+		return (Wohnung) _immobilie;
+	}
+
+	/**
+	 * @param immobilie
+	 *            the immobilie to set
+	 */
+	public void setWohnung(Wohnung wohnung) {
+		this._immobilie = wohnung;
+	}
+
+	/**
+	 * @return the mietbeginn
+	 */
+	public Date getMietbeginn() {
+		return _mietbeginn;
+	}
+
+	/**
+	 * @param mietbeginn
+	 *            the mietbeginn to set
+	 */
+	public void setMietbeginn(Date mietbeginn) {
+		this._mietbeginn = mietbeginn;
+	}
+
+	/**
+	 * @return the dauer
+	 */
+	public int getDauer() {
+		return _dauer;
+	}
+
+	/**
+	 * @param dauer
+	 *            the dauer to set
+	 */
+	public void setDauer(int dauer) {
+		this._dauer = dauer;
+	}
+
+	/**
+	 * @return the nebenkosten
+	 */
+	public float getNebenkosten() {
+		return _nebenkosten;
+	}
+
+	/**
+	 * @param nebenkosten
+	 *            the nebenkosten to set
+	 */
+	public void setNebenkosten(float nebenkosten) {
+		this._nebenkosten = nebenkosten;
+	}
+
+}
