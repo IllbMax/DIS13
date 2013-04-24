e7c761db008802a3a521d28145875746bd893fa0
Max Menzel <catch4all@abwesend.de>
2013-04-24 08:41:16 +0200

    erste SQL fuer Immobilien hinzugefuegt, insert und update fehlen noch

--- Aufgabe 2 eigener Start/src/ersteVersuche/ImmobilienmenuWerkzeug.java
+++ Aufgabe 2 eigener Start/src/ersteVersuche/ImmobilienmenuWerkzeug.java
@@ -2,11 +2,20 @@ package ersteVersuche;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 import javax.swing.event.TableModelEvent;
 import javax.swing.event.TableModelListener;
 
+import dis2011.DB2ConnectionManager;
+import ersteVersuche.Material.Haus;
 import ersteVersuche.Material.Immobilie;
+import ersteVersuche.Material.Wohnung;
 
 public class ImmobilienmenuWerkzeug {
 	private final ImmobilienmenuGUI _GUI;
@@ -48,8 +57,7 @@ public class ImmobilienmenuWerkzeug {
 	private void AddImmobilie() {
 		Immobilie i = _immobilieNeu.ErstelleImmobilie();
 
-		if (i != null) {
-			// TODO add in sql und table
+		if (i != null && AddImmobilieSQL(i)) {
 			_GUI.GetTableModel().AddImmobilie(i);
 		}
 		_GUI.repaint();
@@ -57,8 +65,7 @@ public class ImmobilienmenuWerkzeug {
 
 	private void DelImmobilie(Immobilie i) {
 
-		if (i != null) {
-			// TODO add in sql und table
+		if (i != null && DelImmobilieSQL(i)) {
 			_GUI.GetTableModel().DeleteImmobilie(i);
 		}
 		_GUI.repaint();
@@ -66,8 +73,7 @@ public class ImmobilienmenuWerkzeug {
 
 	private void UpdImmobilie(Immobilie i) {
 
-		if (i != null) {
-			// TODO add in sql (table ist schon fertig :-) )
+		if (i != null && UpdImmobilieSQL(i)) {
 		}
 		_GUI.repaint();
 	}
@@ -79,6 +85,142 @@ public class ImmobilienmenuWerkzeug {
 	public void ZeigeImmobilienMenu() {
 		_GUI.setVisible(true);
 
+	}
+
+	private boolean AddImmobilieSQL(Immobilie i) {
+		Connection con = DB2ConnectionManager.getInstance().getConnection();
+		String selectSQL = "INSERT INTO Makler (Name, Adresse, Login, Passwort) VALUES(?, ?, ?, ?)";
+		PreparedStatement pstmt;
+		try {
+			pstmt = con.prepareStatement(selectSQL);
+			// pstmt.setString(1, i.getName());
+			// pstmt.setString(2, i.getAdresse());
+			// pstmt.setString(3, i.getLogin());
+			// pstmt.setString(4, i.getPasswort());
+			// TODO: INSERT für immobilien
+			// Führe Anfrage aus
+			int rs = pstmt.executeUpdate();
+			return rs > 0;
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+
+	private boolean DelImmobilieSQL(Immobilie i) {
+		Connection con = DB2ConnectionManager.getInstance().getConnection();
+		String selectSQL = "DELETE FROM Immobilie WHERE ID = ?; ";
+		if (i instanceof Haus)
+			selectSQL += " DELETE FROM Haus WHERE ID = ?";
+		if (i instanceof Wohnung)
+			selectSQL += " DELETE FROM Wohnung WHERE ID = ?";
+		PreparedStatement pstmt;
+		try {
+			pstmt = con.prepareStatement(selectSQL);
+			pstmt.setInt(1, i.getID());
+			pstmt.setInt(2, i.getID());
+
+			// Führe Anfrage aus
+			int rs = pstmt.executeUpdate();
+			return rs > 0;
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+
+	private boolean UpdImmobilieSQL(Immobilie i) {
+		Connection con = DB2ConnectionManager.getInstance().getConnection();
+		String selectSQL = "UPDATE Makler SET Name = ?, Adresse = ?, Login = ?, Passwort = ? WHERE Login = ?";
+		PreparedStatement pstmt;
+		try {
+			pstmt = con.prepareStatement(selectSQL);
+			// pstmt.setString(1, i.getName());
+			// pstmt.setString(2, i.getAdresse());
+			// pstmt.setString(3, i.getLogin());
+			// pstmt.setString(4, i.getPasswort());
+
+			// pstmt.setString(5, login);
+
+			// TODO: UPDATE für immobilien
+
+			// Führe Anfrage aus
+			int rs = pstmt.executeUpdate();
+			return rs > 0;
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+		return false;
+	}
+
+	private List<Immobilie> LadeImmobilien() {
+
+		List<Immobilie> result = new ArrayList<Immobilie>();
+		result.addAll(LadeImmobilienHaeuser());
+		result.addAll(LadeImmobilienWohnungen());
+
+		return result;
+	}
+
+	private List<Haus> LadeImmobilienHaeuser() {
+		Connection con = DB2ConnectionManager.getInstance().getConnection();
+
+		List<Haus> result = new ArrayList<Haus>();
+		String selectSQL = "SELECT * FROM Haus JOIN Immobilie ON Id";
+		PreparedStatement pstmt;
+		try {
+			pstmt = con.prepareStatement(selectSQL);
+
+			// Führe Anfrage aus
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				result.add(new Haus(rs.getInt("ID"), rs.getString("Ort"), rs
+						.getString("PLZ"), rs.getString("Strasse"), rs
+						.getString("HausNr"), rs.getFloat("Flaeche"), rs
+						.getInt("Stockwerke"), rs.getFloat("Kaufpreis"), rs
+						.getBoolean("Garten")));
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+		return result;
+	}
+
+	private List<Wohnung> LadeImmobilienWohnungen() {
+		Connection con = DB2ConnectionManager.getInstance().getConnection();
+
+		List<Wohnung> result = new ArrayList<Wohnung>();
+		String selectSQL = "SELECT * FROM Wohnung JOIN Immobilie ON Id";
+		PreparedStatement pstmt;
+		try {
+			pstmt = con.prepareStatement(selectSQL);
+
+			// Führe Anfrage aus
+			ResultSet rs = pstmt.executeQuery();
+
+			while (rs.next()) {
+				result.add(new Wohnung(rs.getInt("ID"), rs.getString("Ort"), rs
+						.getString("PLZ"), rs.getString("Strasse"), rs
+						.getString("HausNr"), rs.getFloat("Flaeche"), rs
+						.getInt("Stockwerk"), rs.getFloat("Mietpreis"), rs
+						.getInt("Zimmer"), rs.getBoolean("Balkon"), rs
+						.getBoolean("EBK")));
+			}
+		} catch (SQLException e) {
+
+			e.printStackTrace();
+		}
+
+		return result;
 	}
 
 }
