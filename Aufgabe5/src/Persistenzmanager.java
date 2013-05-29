40f15f1704b62b77157e2971be9d8bcd68ef31aa
Thorsten <1ploss@informatik.uni-hamburg.de>
2013-05-29 12:25:04 +0200

    Änderungen durchgeführt in der Vorlesung

--- Aufgabe5/src/Persistenzmanager.java
+++ Aufgabe5/src/Persistenzmanager.java
@@ -1,13 +1,13 @@
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
-import java.io.FileReader;
 import java.io.FileWriter;
 import java.io.IOException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
 
 
 
@@ -16,6 +16,8 @@ public class Persistenzmanager {
 	private ArrayList<BufferData> _puffer;
 	private int transaktioncount;
 	private static Persistenzmanager _instance=null;
+	private HashMap<Integer, List<LogData>> _TransactionLogBuffer;
+	
 
 	/**
 	 * voll so krass nen Konstruktor^^
@@ -24,13 +26,14 @@ public class Persistenzmanager {
 		super();
 		_puffer = new ArrayList<BufferData>();
 		transaktioncount = 0;
+		_TransactionLogBuffer = new HashMap<Integer, List<LogData>>();
 	}
 	
 	/**
 	 * Mehtode um das Sigelton-Patter einzuhalten
 	 * @return die einzige Instanz des Persistenzmanagers 
 	 */
-	public static Persistenzmanager getInstance()
+	public synchronized static Persistenzmanager getInstance()
 	{
 		if(_instance== null)
 		{
@@ -45,7 +48,9 @@ public class Persistenzmanager {
 	 */
 	public synchronized int beginTransaktion()
 	{
-		return ++transaktioncount;
+		int TAID = ++transaktioncount;
+		_TransactionLogBuffer.put(TAID, new LinkedList<LogData>());
+		return TAID;
 	}
 	
 	/**
@@ -56,11 +61,25 @@ public class Persistenzmanager {
 	 */
 	public synchronized boolean commit(int taid)
 	{
+		List<LogData> blub = _TransactionLogBuffer.get(taid);
+		for(LogData LD :blub)
+		{
+			LD.SetLOGID(getLogCount()+1);
+			saveLog(LD);
+		}
+		_TransactionLogBuffer.remove(taid);
 		for(BufferData BD : _puffer)
 		{
-			if(BD.GetTAID() == taid)BD.SetCommited(true);
+			if(BD.GetTAID() == taid) BD.SetCommited(true);
 		}
+		
 		return true;
+	}
+	
+	private synchronized int getLogCount()
+	{
+		File directory = new File("LogDaten");
+		return directory.list().length;
 	}
 	
 	/**
@@ -81,18 +100,19 @@ public class Persistenzmanager {
 					usedData = BD;
 				}
 		}
+		LogData boing = new LogData( taid, pageid, data);
+		_TransactionLogBuffer.get(taid).add(boing);
 		if(bufferd)//Fall, dass die Daten schon im Puffer sind
 		{
 			//P(pageid); laut Aufgabenstellung nicht notwendig
-			int logid = saveLog(new LogData( taid, pageid, data));
+			
 			usedData.SetData(data);
-			usedData.SetLOGID(logid);
+			usedData.SetLOGDATA(boing);
 		}
 		else
 		{
 			//P(pageid); laut Aufgabenstellung nicht notwendig
-			int logid = saveLog(new LogData( taid, pageid, data));
-			_puffer.add(new BufferData(taid,pageid,logid,data));
+			_puffer.add(new BufferData(taid,pageid,boing,data));
 		}
 			
 		saveNutz();
@@ -116,18 +136,15 @@ public class Persistenzmanager {
 		}
 	}
 	
-	private int saveLog(LogData logdata)
+	private void saveLog(LogData logdata)
 	{
-		File directory = new File("LogDaten");
-		int anzahlLogdaten = directory.list().length;
-		try {
-			logdata.SetLOGID(anzahlLogdaten+1);
-			
-			PrintWriter pWriter = new PrintWriter(new FileWriter("LogDaten/"+anzahlLogdaten+1));
+		
+		try {			
+			PrintWriter pWriter = new PrintWriter(new FileWriter("LogDaten"+File.separator+logdata.GetLOGID()));
 	        pWriter.print(logdata.toString());
 	        pWriter.flush(); 
 		
-	        return anzahlLogdaten+1;
+	        
 		} catch (FileNotFoundException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
@@ -135,25 +152,15 @@ public class Persistenzmanager {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		return anzahlLogdaten+1;
 	}
 	
 	private void saveNutz(NutzDaten ND)
 	{
+		File file = new File("NutzDaten"+File.separator+ND.GetPAGEID());
 		try {
-			FileOutputStream fis = new FileOutputStream("NutzDaten/"+ND);
-			for(char c : ND.toString().toCharArray())
-			{
-				fis.write((byte)c);
-			}
-		} catch (FileNotFoundException e) {
-			 try{
-		            PrintWriter pWriter = new PrintWriter(new FileWriter("NutzDaten/"+ND));
-		            pWriter.print(ND);
-		            pWriter.flush();
-		        }catch(IOException ioe){
-		            ioe.printStackTrace();
-		        } 
+			PrintWriter pWriter = new PrintWriter(new FileWriter(file));
+            pWriter.print(ND.toString());
+            pWriter.flush();
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
