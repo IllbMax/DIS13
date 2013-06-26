9d4e313c8a80e8f93763e6bf4e62c1d48b498df5
Sakan Baghdasaryan <sakan@dhcp-172-21-3-128.wlan.uni-hamburg.de.local>
2013-06-26 17:32:04 +0200

    land

--- DataWarehouse/src/Shop/LandID.java
+++ DataWarehouse/src/Shop/LandID.java
@@ -1,5 +1,14 @@
 package Shop;
 
 public class LandID {
+	 public static int landID =1;
+	 public static String land ="Deutschland";
+			 
+     public int getLandID()
+     {
+    	 return 1;
+     }
+	 
+	 
 
 }
