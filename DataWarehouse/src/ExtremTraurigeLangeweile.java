528ed4f1a2aefb9f655c43a2841e013bbc6675c3
Thorsten <1ploss@informatik.uni-hamburg.de>
2013-06-26 16:43:55 +0200

    Dinge getan

--- DataWarehouse/src/ExtremTraurigeLangeweile.java
+++ DataWarehouse/src/ExtremTraurigeLangeweile.java
@@ -2,14 +2,16 @@ import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Date;
 
 import Datum.dayID;
+import main.*;
 
 
 public class ExtremTraurigeLangeweile {
 
-	Object[][][] _warehouse = {{{null},{null},{null},{null}},{{null},{null},{null},{null}}};
+	ArrayList<Fakten> faktentabelle = new ArrayList<Fakten>();
 	File csv = new File("../sales.csv");
 	public static void main(String[] args) {
 	
@@ -27,8 +29,8 @@ public class ExtremTraurigeLangeweile {
 	      while( stringRead != null )
 	      {
 	    	  input = stringRead.split(";");
-	    	  behandleDatum(input[0]);
-	    	  dayID.getInstanze(new Date(input[0]));
+	    	  faktentabelle.add(new Fakten(behandleArtikel(input[2]), behandleShop(input[1]), behandleDatum(input[0]), behandleVerkauft(input[3]), behandleUmsatz(input[4])));
+	    	  
 	    	  
 	    	  stringRead = br.readLine();
 	      }
@@ -39,8 +41,25 @@ public class ExtremTraurigeLangeweile {
 	    }
 	}
 	
-	private void behandleDatum(String datum)
+	private dayID behandleDatum(String datum)
 	{
-		
+		return dayID.getInstanze(new Date(datum));
+	}
+	
+	private ArtikelID behandleArtikel(String artikel)
+	{
+		return ArtikelID.getInstanze(artikel);
+	}
+	
+	private ShopID behandleShop(String shop) {
+		return ShopID.getInstanze(shop);
+	}
+	
+	private int behandleVerkauft(String verkauft) {
+		return Integer.parseInt(verkauft);
+	}
+	
+	private double behandleUmsatz(String umsatz) {
+		return Double.parseDouble(umsatz);
 	}
 }
