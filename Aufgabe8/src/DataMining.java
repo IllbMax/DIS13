9a16938253edef5ba5317128194f32149fef8cff
Max Menzel <catch4all@abwesend.de>
2013-07-03 16:48:40 +0200

    muh

---
+++ Aufgabe8/src/DataMining.java
@@ -0,0 +1,74 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.Reader;
+import java.util.ArrayList;
+import java.util.HashMap;
+
+public class DataMining {
+
+	File daten = new File("./transactions.txt");
+	Reader is;
+	ArrayList<String[]> zeilen;
+	HashMap<Integer, Integer> itemList;
+
+	public static void main(String[] args) {
+		DataMining dm = new DataMining();
+		System.out.println(dm.findFrequent1Itemsets());
+
+	}
+
+	public DataMining() {
+
+		zeilen = new ArrayList<String[]>();
+		itemList = new HashMap<Integer, Integer>();
+		try {
+			is = new FileReader(daten);
+
+			BufferedReader br = new BufferedReader(is);
+			String zeile = "";
+			do {
+				zeile = br.readLine();
+				if (zeile != null) {
+					String[] items = zeile.split(" ");
+					zeilen.add(items);
+				}
+
+			} while (zeile != null);
+
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
+
+	public ArrayList<Integer> findFrequent1Itemsets() {
+		int grossT = zeilen.size();
+
+		for (String[] sarray : zeilen) {
+			for (String s : sarray) {
+
+				int itemnumber = Integer.parseInt(s);
+				if (!itemList.containsKey(itemnumber)) {
+					itemList.put(itemnumber, 1);
+
+				} else {
+					int count = itemList.get(itemnumber);
+					count++;
+
+					itemList.put(itemnumber, count);
+				}
+
+			}
+		}
+		ArrayList<Integer> result = new ArrayList<Integer>();
+		for (int dingens : itemList.keySet()) {
+			System.out.println((double) (itemList.get(dingens) / grossT));
+			if (((double) itemList.get(dingens)) / grossT >= 0.01) {
+				result.add(dingens);
+			}
+		}
+		return result;
+	}
+
+}
